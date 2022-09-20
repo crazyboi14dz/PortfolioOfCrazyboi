70da6dcbb733f4982bad4d294307f5b07dceeafb
crazyboi14dz <dilipkumarsahoo2003@gmail.com>
2022-09-20 13:18:57 +0530

    maked an navbar and footer and in JS DOMContentLoaded is used.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,46 @@
-const dynamicContent = document.getElementById("dynamic-text")
-console.log(dynamicContent)
+document.addEventListener("DOMContentLoaded", () => {
 
-const phrases = ["Student...","Explorer...","Human Being..."]
+    const dynamicContent = document.getElementById("dynamic-text")
+    // console.log(dynamicContent)
 
-let phraseIndex = 0;
-let letterIndex = 0;
-const typingSpeed = 100;
-const erasingSpeed = 100;
+    const phrases = ["Student...", "Explorer...", "Human Being..."]
 
-function printLetters(phrase) {
-    if (letterIndex == phrase.length) {
-        //clear letter which have been type
-        clearLetters();
-    } else if (letterIndex < phrase.length) {
-        dynamicContent.textContent += phrase.charAt(letterIndex);
-        letterIndex += 1;
-        //function ref,100ms(1000ms=1s)
-        setTimeout(function () {
-            printLetters(phrase)
-        }, typingSpeed)
-    }
-}
+    let phraseIndex = 0;
+    let letterIndex = 0;
+    const typingSpeed = 100;
+    const erasingSpeed = 100;
 
-
-function clearLetters() {
-    if (letterIndex == -1) {
-        phraseIndex = (phraseIndex + 1) % phrases.length;
-        letterIndex = 0;
-        printLetters(phrases[phraseIndex])
-    }
-    else if (letterIndex > -1) {
-        let updatedPhrase = "";
-        for (let index = 0; index < letterIndex; index++) {
-            updatedPhrase += phrases[phraseIndex].charAt(index);
+    function printLetters(phrase) {
+        if (letterIndex == phrase.length) {
+            //clear letter which have been type
+            clearLetters();
+        } else if (letterIndex < phrase.length) {
+            dynamicContent.textContent += phrase.charAt(letterIndex);
+            letterIndex += 1;
+            //function ref,100ms(1000ms=1s)
+            setTimeout(function () {
+                printLetters(phrase)
+            }, typingSpeed)
         }
-        console.log(updatedPhrase);
-        dynamicContent.textContent = updatedPhrase;
-        letterIndex -= 1;
-        setTimeout(clearLetters, erasingSpeed);
     }
-}
 
-printLetters(phrases[phraseIndex])
+
+    function clearLetters() {
+        if (letterIndex == -1) {
+            phraseIndex = (phraseIndex + 1) % phrases.length;
+            letterIndex = 0;
+            printLetters(phrases[phraseIndex])
+        } else if (letterIndex > -1) {
+            let updatedPhrase = "";
+            for (let index = 0; index < letterIndex; index++) {
+                updatedPhrase += phrases[phraseIndex].charAt(index);
+            }
+            console.log(updatedPhrase);
+            dynamicContent.textContent = updatedPhrase;
+            letterIndex -= 1;
+            setTimeout(clearLetters, erasingSpeed);
+        }
+    }
+
+    printLetters(phrases[phraseIndex])
+})
